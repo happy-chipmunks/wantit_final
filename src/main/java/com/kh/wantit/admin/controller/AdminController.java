269b0491f81dac13aa84c65abe18139189df9d3d
DESKTOP-GGGHBJM\주인 <jinhwankwon@naver.com>
2022-12-12 17:22:22 +0900

    자잘한 코드와 전화번호 이메일 enroll 바꾸기

--- src/main/java/com/kh/wantit/admin/controller/AdminController.java
+++ src/main/java/com/kh/wantit/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.kh.wantit.admin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.wantit.admin.model.service.AdminService;
 
@@ -9,5 +10,11 @@ import com.kh.wantit.admin.model.service.AdminService;
 public class AdminController {
 	@Autowired
 	private AdminService aService;
-
+	
+	@RequestMapping("/admin.ad")
+	public String admin() {
+		return "adminMainpage";
+	}
+	
+	
 }
