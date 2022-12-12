7791a170df4e03108b5b7f569c10fb12ff60dd34
jarsenal1074 <jarsenal1074@gmail.com>
2022-12-12 16:57:19 +0900

    member 서비스 서비스임플 DAO 삭제(오류나서)

---
+++ src/main/java/com/kh/wantit/member/memberController.java
@@ -0,0 +1,93 @@
+package com.kh.wantit.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class memberController {
+	
+	
+	
+	@RequestMapping("/memberenroll.me")
+	   public String enroll() {
+	      return "myPage_enroll";
+	   }
+	   
+	   @RequestMapping("/myPageSupporter.me")
+	   public String myPageSupporter() {
+	      return "myPage_sup";
+	   }
+	   
+	   @RequestMapping("/myPageSupporterFunding.me")
+	   public String myPageSupporterFunding() {
+	      return "myPage_sup_funding";
+	   }
+	   
+	   @RequestMapping("/myPageSupporterWanting.me")
+	   public String myPageSupporterWanting() {
+	      return "myPage_sup_wanting";
+	   }
+	   
+	   @RequestMapping("/myPageSupporterDibs.me")
+	   public String myPageSupporterDibs() {
+	      return "myPage_sup_dibs";
+	   }
+	   
+	   @RequestMapping("/myPageSupporterInquiry.me")
+	   public String myPageSupporterInquiry() {
+	      return "myPageSupporterInquiry";
+	   }
+	   
+	   @RequestMapping("/myPageSupporterAlarm.me")
+	   public String myPageSupporterAlarm() {
+	      return "myPage_sup_alarm";
+	   }
+	   
+	   @RequestMapping("/myPageSupporterFollow.me")
+	   public String myPageSupporterFollow() {
+	      return "myPage_sup_follow";
+	   }
+	   
+	   @RequestMapping("/myPageSupporterPayList.me")
+	   public String myPageSupporterPayList() {
+	      return "myPage_sup_payList";
+	   }
+	   
+	   @RequestMapping("/myPageCreator.me")
+	   public String myPageCreator() {
+	      return "myPage_creator";
+	   }
+	   
+	   @RequestMapping("/myPageCreatorFunding.me")
+	   public String myPageCreatorFunding() {
+	      return "myPage_creator_funding";
+	   }
+	   
+	   @RequestMapping("/myPageCreatorInquiry.me")
+	   public String myPageCreatorInquiry() {
+	      return "myPage_creator_inquiry";
+	   }
+	   
+	   @RequestMapping("/myPageCreatorAlarm.me")
+	   public String myPageCreatorAlarm() {
+	      return "myPage_creator_alarm";
+	   }
+	   
+	   @RequestMapping("/myPageCreatorFollow.me")
+	   public String myPageCreatorFollow() {
+	      return "myPage_creator_follow";
+	   }
+	   
+	   @RequestMapping("/myPageCreatorAds.me")
+	   public String myPageCreatorAds() {
+	      return "myPage_creator_ads";
+	   }
+	   
+	
+	
+}
